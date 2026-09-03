f53d24d61d8b8b4c276834bba5cc1a57eca98c8b
Matheus Henrique <137726710+Cirker1226@users.noreply.github.com>
2026-09-03 20:14:25 -0300

    Atualização do Crud

--- site/server.js
+++ site/server.js
@@ -1,10 +1,21 @@
-fetch ('/produtos')
-    .then (res => res.json())
-    .then (data => {
-        data.forEach(element => {
-            document.getElementById("elements").innerHTML += `<td>${element.categoria}</td>`
-            document.getElementById("elements").innerHTML += `<td>${element.descricao}</td>`
-            document.getElementById("elements").innerHTML += `<td>${element.preco}</td>`
-            document.getElementById("elements").innerHTML += `<td>${element.estoque}</td>`
-        });
-    });
+function carregarProdutos() {
+    fetch('/produtos')
+        .then(res => res.json())
+        .then(data => {
+            const tabela = document.getElementById("elementos");
+
+            data.forEach(element => {
+                tabela.innerHTML += `
+                    <tr>
+                        <td>${element.categoria}</td>
+                        <td>${element.descricao}</td>
+                        <td>${element.preco}</td>
+                        <td>${element.estoque}</td>
+                    </tr>
+                `;
+            });
+        })
+}
+
+carregarProdutos();
+
